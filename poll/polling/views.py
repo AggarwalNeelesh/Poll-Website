04bb974cd3e96229b5dc2a7e635518fb4e1036d2
Neelesh Aggarwal <86107529+AggarwalNeelesh@users.noreply.github.com>
2023-08-29 20:04:32 +0530

    UI enhanced and bugs fixed

--- poll/polling/views.py
+++ poll/polling/views.py
@@ -5,6 +5,10 @@ from django.contrib.auth import authenticate, login as loginUser, logout as logo
 
 
 def login(request):
+    # Super users are not common users
+    if request.user.is_superuser:
+        logout(request)
+
     # If user already logged in
     if request.user.is_authenticated:
         return redirect('/homepage')
@@ -14,8 +18,9 @@ def login(request):
         password = request.POST.get('password')
 
         userauth = authenticate(username=username, password=password)
+        # Checking if user is registered or not
         if userauth is not None:
-            # A backend authenticated the credentials
+            # logging in the user
             loginUser(request, userauth)
             return redirect('/homepage')
         else:
@@ -23,22 +28,6 @@ def login(request):
             return render(request, 'polling/login.html')
 
     return render(request, 'polling/login.html')
-    #
-    #     user_list = User.objects.filter(username=username)
-    #     # When User was not found
-    #     if len(user_list) == 0:
-    #         return render(request, 'polling/login.html', {"UserNotFound": True})
-    #     if password != user_list[0].password:
-    #         return render(request, 'polling/login.html', {"IncorrectPassword": True})
-    #
-    #     # Now we will go to home page
-    #     global Uname
-    #     Uname = username
-    #
-    #     Ulogin = True
-    #     return redirect('/homepage')
-    # else:
-    #     return render(request, 'polling/login.html')
 
 
 def register(request):
@@ -52,30 +41,36 @@ def register(request):
         # When Username was already used
         if len(user_list) > 0:
             return render(request, 'polling/login.html', {"UserAlreadyRegistered": True})
-        # Creating User
+        # Creating our custom User
         user = User(name=name, username=username, email=email, phone=phone, password=password, questions=0)
         user.save()
-
+        # creating and saving the user of authentication
         userauth = UserAuth.objects.create_user(username=username, password=password)
         userauth.save()
-
-        login(request)
-        return render(request, 'polling/login.html')
+        # redirecting user to logout page
+        # If any user was previously logged in then it will be first logged out then login again.
+        return redirect('/logout')
     else:
         return render(request, 'polling/register.html')
 
 
 def logout(request):
+    # logging out user
     logoutUser(request)
     return redirect('/login')
 
 
 def homepage(request):
-    Uname = request.user.username
-    if request.user.is_authenticated:
-        tag = 'All'
+    # Super users are not common users
+    if request.user.is_superuser:
+        return redirect('/logout')
 
+    # Checking if user is authenticated
+    if request.user.is_authenticated:
+        # By default Questions from all tags are displayed in homepage
+        tag = 'All'
         if request.method == 'POST':
+            # if a particular tag request has arrived
             tag = request.POST.get('tag') or 'All'
 
         if tag == 'All':
@@ -83,29 +78,35 @@ def homepage(request):
         else:
             allqueslist = Question.objects.filter(tag=tag)
 
-        u = User.objects.filter(username=Uname)[0]
+        # Checking if user has already voted the question in question list
+        u = User.objects.filter(username=request.user.username)[0]
         for i in allqueslist:
             votes_status = Votes.objects.filter(user_id=u.user_id, Q_id=i.Q_id)
             if len(votes_status) != 0:
                 # Voted Already
                 setattr(i, 'alreadyVoted', True)
-            # else:
-            #     setattr(i, 'AlreadyVoted', False)
 
-        # print(type(allqueslist[0].AlreadyVoted))
+        # Fetching List of all available tags to be displayed in homepage
         allTags = Question.objects.values('tag').distinct()
-        return render(request, 'polling/homepage.html', {'quesList': allqueslist, 'username': Uname,
+        return render(request, 'polling/homepage.html', {'quesList': allqueslist, 'username': request.user.username,
                                                          'allTags': allTags})
     else:
         return render(request, 'polling/login.html')
 
 
 def profile(request):
-    Uname = request.user.username
+    # Super users are not common users
+    if request.user.is_superuser:
+        return redirect('/logout')
+
+    # Checking if user is authenticated
     if request.user.is_authenticated:
-        u = User.objects.filter(username=Uname)[0]
-        print(Uname, u.name)
+        u = User.objects.filter(username=request.user.username)[0]
         allqueslist = Question.objects.filter(user_id=u.user_id)
+
+        for i in allqueslist:
+            setattr(i, 'alreadyVoted', True)
+
         dic = {"name": u.name, "email": u.email, "phone": u.phone, "question": u.questions, 'quesList': allqueslist}
         return render(request, 'polling/profile.html', dic)
     else:
@@ -113,16 +114,19 @@ def profile(request):
 
 
 def createquestion(request):
-    Uname = request.user.username
+    # Super users are not common users
+    if request.user.is_superuser:
+        return redirect('/logout')
+
+    # Checking if user is authenticated
     if request.user.is_authenticated:
         if request.method == 'POST':
-            us = User.objects.filter(username=Uname)
-            print(Uname, us)
-            if len(us) == 0:
-                return render(request, 'polling/login.html')
-            u = us[0]
+            # Fetching user
+            u = User.objects.filter(username=request.user.username)[0]
+            # A user can create maximum of 5 questions
             if u.questions >= 5:
                 return render(request, 'polling/createquestion.html', {"LimitExceeded": True})
+            # Updating user details
             u.questions += 1
             u.save()
             ques = request.POST.get('ques')
@@ -133,26 +137,29 @@ def createquestion(request):
             opt4 = request.POST.get('opt4')
             question = Question(question=ques, op1=opt1, op2=opt2, op3=opt3, op4=opt4, user_id=u.user_id, tag=tag)
             question.save()
-            allqueslist = Question.objects.all()
-            return render(request, 'polling/homepage.html', {'quesList': allqueslist})
+            return redirect('/homepage')
         return render(request, 'polling/createquestion.html')
     else:
         return render(request, 'polling/login.html')
 
 
 def votes(request):
+    # Super users are not common users
+    if request.user.is_superuser:
+        return redirect('/logout')
+
+    # Checking if user is not authenticated
     if not request.user.is_authenticated:
         return render(request, 'polling/login.html')
     # Method to check that which user has voted for the question
     if request.method == 'POST':
-        Uname = request.user.username
         # Selected Option by User is op
         op = request.POST.get('op')
         qid = request.POST.get('qid')
-        u = User.objects.filter(username=Uname)[0]
+        u = User.objects.filter(username=request.user.username)[0]
         ques = Question.objects.filter(Q_id=qid)[0]
 
-        # Fetching If the user has voted this Q already
+        # Fetching If the user has voted this Question already
         votes_status = Votes.objects.filter(user_id=u.user_id, Q_id=qid)
 
         if len(votes_status) == 0:
@@ -172,3 +179,4 @@ def votes(request):
             ques.save()
 
     return redirect('/homepage')
+
